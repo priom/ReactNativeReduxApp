3c38afa71daf2bc3b674fd27af9a27c00b1505fe
priom <priom.galaxy@gmail.com>
2017-02-17 02:44:49 -0500

    components added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+
 
 const App = () => {
     return (
-        <View></View>
+        <Provider store={createStore(reducers)}>
+            <View>
+                <Header headerText="TechStack"></Header>
+            </View>
+        </Provider>
     );
 };
 
